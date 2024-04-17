ff34c5e8341e9c4373ce05fa5936da370922edb3
Marcin Gwóźdź <marcin.gwozdz@testto.com>
2024-04-17 18:14:13 +0200

    Initialize base project structure and configuration
    This commit adds the necessary Maven configuration file (pom.xml), Spring Boot main application and tests, and application properties. Additionally, a .gitignore file is added to ignore undesired files and directories generated by various IDEs and build tools. An ASCII art banner was added to provide a welcoming command line display.

---
+++ src/main/java/com/dungeonclicker/DungeonClickerApplication.java
@@ -0,0 +1,13 @@
+package com.dungeonclicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonClickerApplication.class, args);
+	}
+
+}
